ff171fe56259d03ba597c44468eb6873cfeaed30
Stan Borzhemsky <stanbsky@protonmail.com>
2022-04-17 16:29:55 +0100

    Add Debug.debug() static method for printing to console every x update() calls

---
+++ deltaducks/core/src/com/ducks/tools/Debug.java
@@ -0,0 +1,34 @@
+package com.ducks.tools;
+
+
+import java.util.ArrayList;
+
+/**
+ * Used to print messages to console with a given frequency,
+ * displaying the name of the class registering the message.
+ */
+public final class Debug {
+    private static int ticks = 0;
+    private final static int tickFrequency = 60;
+    private static StackWalker walker;
+    private static ArrayList<String> messages;
+
+    public static void Initialize() {
+        walker = StackWalker.getInstance(StackWalker.Option.RETAIN_CLASS_REFERENCE);
+        messages = new ArrayList<>();
+    }
+
+    public static void debug(Object message) {
+        messages.add(walker.getCallerClass() + " : " + message);
+    }
+
+    public static void update() {
+        ticks++;
+        if (ticks == tickFrequency) {
+            ticks = 0;
+            for (String message : messages) {
+                System.out.println(message);
+            }
+        }
+    }
+}
