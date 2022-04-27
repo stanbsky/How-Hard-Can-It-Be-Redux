90f235f9f9dff6c3bf67d158395a714aa64bfb15
Stan Borzhemsky <stanbsky@protonmail.com>
2022-04-27 02:37:00 +0100

    Finalise the method for writing tests, adding a template and an example test

--- deltaducks/tests/src/de/tomgrill/gdxtesting/TestTemplate.java
+++ deltaducks/tests/src/de/tomgrill/gdxtesting/TestTemplate.java
@@ -1,39 +1,34 @@
 package de.tomgrill.gdxtesting;
 
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.backends.headless.HeadlessApplication;
 import com.badlogic.gdx.backends.headless.HeadlessApplicationConfiguration;
-import com.badlogic.gdx.graphics.g2d.TextureAtlas;
+import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.math.Vector2;
-import com.badlogic.gdx.physics.box2d.Contact;
 import com.badlogic.gdx.physics.box2d.World;
-import com.ducks.intangibles.Quest;
+import com.ducks.managers.AssetManager;
 import com.ducks.managers.PhysicsManager;
 import com.ducks.tools.EntityContactListener;
-import com.ducks.ui.Subtitle;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import static org.mockito.Mockito.mock;
+
 @ExtendWith(MockitoExtension.class)
-public class QuestTests {
+public class TestTemplate {
+
     private static DDHeadless game;
     private static HeadlessApplication app;
     private World world;
-    private final Vector2 zero = new Vector2(0, 0);
     private EntityContactListener contactListener;
-    @Mock
-    TextureAtlas atlas;
-    @Mock
-    BulletManager cannons;
-    @Mock
-    Contact contact;
-    @Mock Subtitle sub;
+    private final Vector2 zero = new Vector2(0, 0);
+    private final float deltaTime = 1/60f;
 
     @BeforeAll
     public static void setupApp() {
+        Gdx.gl = mock(GL20.class);
         HeadlessApplicationConfiguration conf = new HeadlessApplicationConfiguration();
         conf.updatesPerSecond = 60;
         game = new DDHeadless();
@@ -44,10 +39,6 @@ public class QuestTests {
         world = new World(new Vector2(0,0), true);
         contactListener = new EntityContactListener();
         PhysicsManager.Initialize(world);
-    }
-    @Test
-    public void testCreateQuest() {
-//        when(sub.)
-        Quest quest = new Quest("chest", zero, sub, atlas);
+        AssetManager.Initialize();
     }
 }
