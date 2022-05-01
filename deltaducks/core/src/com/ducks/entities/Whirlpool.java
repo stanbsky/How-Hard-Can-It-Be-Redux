ec452fe9d8a7feb387f9e4311a64c7b0a3554d89
Kroojel <joesexton123@outlook.com>
2022-05-01 17:52:31 +0100

    test_SCREEN added in the wrong file

--- deltaducks/core/src/com/ducks/entities/Whirlpool.java
+++ deltaducks/core/src/com/ducks/entities/Whirlpool.java
@@ -41,6 +41,7 @@ public class Whirlpool extends Entity {
 
     public void update(float deltaTime) {
         super.update(deltaTime);
+        timer--;
         pullEntities();
         texture.update(deltaTime, position);
     }
